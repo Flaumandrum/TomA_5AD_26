8b3cd94d5ad7a776d14564026a790e88f280b280
tomadriaens <tomadriaens@hotmail.com>
2026-09-22 10:47:56 +0200

    [LES] Oef 03 afgewerkt

---
+++ 02_HalloWereld/02_HalloWereld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_HalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Tom Adriaens
+             * 18/09/2026
+             * Hallo Wereld
+             */
+
+            // Velden
+
+            // Programma
+            // Stap 1: Toon de tekst
+            Console.WriteLine("Hallo wereld!");
+
+            // Stap 2: wacht tot de gebruiker op een toets heeft gedrukt.
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 03_TomA_Spreuken/03_TomA_Spreuken/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_TomA_Spreuken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // 18/09/2026
+            // Spreuken
+
+            // Velden 
+
+            // Programma 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Gray;
+
+            // Stap 1: Toon zin 1
+            Console.WriteLine("Met hard werken en streng zijn voor jezelf, kom je heel ver in het leven.");
+            Console.WriteLine();
+
+            // Stap 2: vraag om op een toets te drukken
+            Console.WriteLine("Druk op een toets om verder te gaan\n");
+            Console.ReadKey();
+
+            // Scherm wissen 
+            Console.Clear();
+
+            // Stap 3: Toon zin 2
+            Console.WriteLine("Een diploma zorgt ervoor dat je aan een hoger loon start. \n");
+
+            // Stap 4: vraag om op een toets te drukken
+            Console.WriteLine("Druk op een toets om verder te gaan\n");
+            Console.ReadKey();
+
+            // Scherm wissen 
+            Console.Clear();
+
+            // Stap 5: Toon zin 3
+            Console.WriteLine("Programmeren is eerst analyseren in kleine stapjes");
+
+
+
+        }
+    }
+}
